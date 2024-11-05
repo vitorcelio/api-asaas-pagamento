606fb3dec16e3732af855567a73c8b602a356085
vitorcelio <celiovitor62@gmail.com>
2024-11-05 02:08:24 -0300

    Consumindo serviço de Assinaturas

--- src/test/java/com/asaas/docs/service/subscription/SubscriptionTest.java
+++ src/test/java/com/asaas/docs/service/subscription/SubscriptionTest.java
@@ -1,14 +1,11 @@
 package com.asaas.docs.service.subscription;
 
 import com.asaas.docs.configuration.AsaasApiConfig;
-import com.asaas.docs.dto.request.SubscriptionRequestDTO;
-import com.asaas.docs.dto.request.SubscriptionUpdateRequestDTO;
-import com.asaas.docs.dto.response.DeleteResponseDTO;
-import com.asaas.docs.dto.response.PaymentsListResponseDTO;
-import com.asaas.docs.dto.response.SubscriptionResponseDTO;
-import com.asaas.docs.dto.response.SubscriptionsListResponseDTO;
+import com.asaas.docs.dto.request.*;
+import com.asaas.docs.dto.response.*;
 import com.asaas.docs.enums.BillingType;
 import com.asaas.docs.enums.CycleType;
+import com.asaas.docs.enums.InvoiceCreationPeriod;
 import com.asaas.docs.enums.StatusSubscription;
 import com.asaas.docs.service.subscription.impl.SubscriptionServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -114,6 +111,104 @@ public class SubscriptionTest {
         String response = subscriptionService.generateSubscriptionBooklet("sub_u4c7g6d86ua100fg", 1, 2025, null, null);
 
         assertNotNull(response);
+    }
+
+    @Test
+    public void testCreateConfigurationForIssuingInvoice_Success() {
+
+        ConfigurationIssuingInvoicesRequestDTO request = ConfigurationIssuingInvoicesRequestDTO.builder()
+                .municipalServiceId("teste id")
+                .municipalServiceCode("teste code")
+                .municipalServiceName("teste name")
+                .updatePayment(false)
+                .deductions(new BigDecimal("50.00"))
+                .effectiveDatePeriod(InvoiceCreationPeriod.ON_PAYMENT_DUE_DATE)
+                .receivedOnly(false)
+                .daysBeforeDueDate(3)
+                .observations("Testando observations")
+                .taxes(TaxesRequestDTO.builder()
+                        .retainIss(false)
+                        .cofins(new BigDecimal("20"))
+                        .csll(new BigDecimal("1"))
+                        .inss(new BigDecimal("2"))
+                        .ir(new BigDecimal("1"))
+                        .pis(new BigDecimal("1"))
+                        .iss(new BigDecimal("1"))
+                        .build())
+                .build();
+
+        ConfigurationIssuingInvoicesResponseDTO response = subscriptionService.createConfigurationForIssuingInvoice("sub_h6lixs41o485n32d", request);
+
+        assertEquals(ConfigurationIssuingInvoicesResponseDTO.class, response.getClass());
+        assertNotNull(response);
+        assertEquals("teste id", response.getMunicipalServiceId());
+        assertEquals("teste code", response.getMunicipalServiceCode());
+        assertEquals("teste name", response.getMunicipalServiceName());
+        assertEquals("Testando observations", response.getObservations());
+
+    }
+
+    @Test
+    public void testGetConfigurationForIssuingInvoice_Success() {
+
+        ConfigurationIssuingInvoicesResponseDTO response = subscriptionService.getConfigurationForIssuingInvoice("sub_h6lixs41o485n32d");
+
+        assertEquals(ConfigurationIssuingInvoicesResponseDTO.class, response.getClass());
+        assertNotNull(response);
+        assertEquals("teste id", response.getMunicipalServiceId());
+        assertEquals("teste code", response.getMunicipalServiceCode());
+        assertEquals("teste name", response.getMunicipalServiceName());
+        assertEquals("Testando observations", response.getObservations());
+
+    }
+
+    @Test
+    public void testUpdateConfigurationForIssuingInvoice_Success() {
+
+        ConfigurationUpdateIssuingInvoicesRequestDTO request = ConfigurationUpdateIssuingInvoicesRequestDTO.builder()
+                .deductions(new BigDecimal("45.00"))
+                .receivedOnly(true)
+                .effectiveDatePeriod(InvoiceCreationPeriod.ON_PAYMENT_DUE_DATE)
+                .taxes(TaxesRequestDTO.builder()
+                        .retainIss(false)
+                        .cofins(new BigDecimal("2"))
+                        .csll(new BigDecimal("3"))
+                        .inss(new BigDecimal("2"))
+                        .ir(new BigDecimal("4"))
+                        .pis(new BigDecimal("2"))
+                        .iss(new BigDecimal("2"))
+                        .build())
+                .build();
+
+        ConfigurationIssuingInvoicesResponseDTO response = subscriptionService.updateConfigurationForIssuingInvoice("sub_h6lixs41o485n32d", request);
+        assertEquals(ConfigurationIssuingInvoicesResponseDTO.class, response.getClass());
+        assertNotNull(response);
+        assertEquals("teste id", response.getMunicipalServiceId());
+        assertEquals("teste code", response.getMunicipalServiceCode());
+        assertEquals("teste name", response.getMunicipalServiceName());
+        assertEquals("Testando observations", response.getObservations());
+    }
+
+    @Test
+    public void testInvoicesForSubscriptionList_Success() {
+
+        InvoicesListResponseDTO response = subscriptionService.invoicesForSubscriptionList("sub_h6lixs41o485n32d", null, null, null, null, null, null,
+                null);
+
+        assertEquals(InvoicesListResponseDTO.class, response.getClass());
+        assertNotNull(response);
+
+    }
+
+    @Test
+    public void testDeleteConfigurationForIssuingInvoice_Success() {
+
+        DeleteResponseDTO response = subscriptionService.deleteConfigurationForIssuingInvoice("sub_h6lixs41o485n32d");
+
+        assertEquals(DeleteResponseDTO.class, response.getClass());
+        assertNotNull(response);
+        assertTrue(response.isDeleted());
+
     }
 
 }
